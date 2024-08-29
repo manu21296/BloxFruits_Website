a4b90762a6ccc23843dd6ed53bd99bf69db59f20
manu21296 <21296@burnside,school.nz>
2024-08-29 12:34:24 +1200

    added comments and new banner

--- routes.py
+++ routes.py
@@ -3,11 +3,13 @@ import sqlite3
 
 app = Flask(__name__)
 
+# connecting to the database
 def get_db_connection():
     conn = sqlite3.connect('DF_DATABASE.db')
     conn.row_factory = sqlite3.Row
     return conn
 
+# it retrieves the categories from the 'Categories' table
 @app.context_processor
 def inject_categories():
     conn = get_db_connection()
@@ -15,6 +17,7 @@ def inject_categories():
     conn.close()
     return {'nav_categories': categories}
 
+# this function opens a connection to the database, executes a query to get all categories from the 'Categories' table, and then closes the connection.
 @app.route('/')
 def index():
     conn = get_db_connection()
@@ -22,6 +25,7 @@ def index():
     conn.close()
     return render_template('index.html', categories=categories)
 
+# this function takes a category ID from the URL, retrieves the category details and associated devil fruits from the database, and then closes the connection.
 @app.route('/category/<int:category_id>')
 def category(category_id):
     conn = get_db_connection()
@@ -30,6 +34,7 @@ def category(category_id):
     conn.close()
     return render_template('category.html', category=category, fruits=fruits)
 
+# this function takes a fruit ID from the URL, retrieves the details of the devil fruit and its associated abilities from the database, and then closes the connection.
 @app.route('/fruit/<int:fruit_id>')
 def fruit(fruit_id):
     conn = get_db_connection()
@@ -38,6 +43,7 @@ def fruit(fruit_id):
     conn.close()
     return render_template('fruit.html', fruit=fruit, abilities=abilities)
 
+# this function retrieves all user records from the 'Users' table in the database and closes the database connection
 @app.route('/users')
 def users():
     conn = get_db_connection()
@@ -45,5 +51,6 @@ def users():
     conn.close()
     return render_template('users.html', users=users)
 
+#
 if __name__ == '__main__':
     app.run(debug=True)
